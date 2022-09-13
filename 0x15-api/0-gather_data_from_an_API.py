11c0bd87a9ca97279723c68f5c2a3c0339341ad3
Aleph235 <akram239911@gmail.com>
2022-09-13 13:53:07 +0200

    if name

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,34 +10,34 @@ if __name__ == "__main__":
         exit(1)
 
 
-baseurl = "https://jsonplaceholder.typicode.com/"
-endpoint = "users/{}".format(id)
+    baseurl = "https://jsonplaceholder.typicode.com/"
+    endpoint = "users/{}".format(id)
 
 
-def main_request(baseurl, endpoint):
-    """request function"""
-    r = requests.get(baseurl + endpoint)
-    return r.json()
+    def main_request(baseurl, endpoint):
+        """request function"""
+        r = requests.get(baseurl + endpoint)
+        return r.json()
 
 
-user_info = main_request(baseurl, endpoint)
-user_todos = main_request(baseurl, endpoint+'/todos')
+    user_info = main_request(baseurl, endpoint)
+    user_todos = main_request(baseurl, endpoint+'/todos')
 
-name = user_info['name']
+    name = user_info['name']
 
-done_title = []
+    done_title = []
 
 
-for user_todo in user_todos:
-    if(user_todo['completed'] is True):
-        done_title.append(user_todo['title'])
+    for user_todo in user_todos:
+        if(user_todo['completed'] is True):
+            done_title.append(user_todo['title'])
 
-number_of_todos = len(user_todos)
-done_number = len(done_title)
+    number_of_todos = len(user_todos)
+    done_number = len(done_title)
 
-print(done_number)
-print("Employee {} is done with tasks({}/{}):".format(
-    name, done_number, number_of_todos))
+    print(done_number)
+    print("Employee {} is done with tasks({}/{}):".format(
+        name, done_number, number_of_todos))
 
-for item in done_title:
-    print('\t ' + str(item))
+    for item in done_title:
+        print('\t ' + str(item))
