671728a2a23f50e984a049e98dc6407922051f35
Aleph235 <akram239911@gmail.com>
2022-09-28 11:39:49 +0200

    api advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Subreddit api top ten'''
+
+def top_ten(subreddit):
+    '''Subreddit to ten info'''
+    import requests
+
+    response = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "zamacode-zama"},
+                            params={"limit": "10"},
+                            allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    
+    for item in response.json().get("data").get("children"):
+        print(item.get("data").get("title"))
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Subreddit api top ten'''
+
+def recurse(subreddit, hot_list=[], after=""):
+    '''Subreddit to ten info'''
+    import requests
+
+    response = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "zamacode-zama"},
+                            params={"after": after},
+                            allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    
+    for item in response.json().get("data").get("children"):
+        hot_list.append(item.get("data").get("title"))
+    after = response.json().get("data").get("after")
+    if after is not None:
+        recurse(subreddit, hot_list, after= after)
+    return(hot_list)
