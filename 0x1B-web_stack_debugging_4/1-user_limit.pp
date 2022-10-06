fa9ad64c30e0d512fd1273b0f4dfb76be9cc9af3
Aleph235 <akram239911@gmail.com>
2022-10-06 13:00:02 +0200

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,6 +3,6 @@
 exec { 'increase-hard-file-limit':
   command => "/bin/sed -i '/holberton hard/s/5/50000/' /etc/security/limits.conf",
 }
-exec { 'increase-hard-file-limit':
+exec { 'increase-soft-file-limit':
   command => "/bin/sed -i '/holberton soft/s/4/50000/' /etc/security/limits.conf",
 }
