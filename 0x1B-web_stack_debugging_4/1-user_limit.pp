6a2464fe4df9824008c33396888c73d63bc7651d
Aleph235 <akram239911@gmail.com>
2022-10-06 12:59:35 +0200

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,8 @@
 # Enable the user holberton to login and open files without error.
 
 exec { 'increase-hard-file-limit':
-  command => '/bin/sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  command => "/bin/sed -i '/holberton hard/s/5/50000/' /etc/security/limits.conf",
 }
 exec { 'increase-hard-file-limit':
-  command => '/bin/sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  command => "/bin/sed -i '/holberton soft/s/4/50000/' /etc/security/limits.conf",
 }
