0f2a38f06a553a4e7ce136f13bd386d8ac97e8a0
Aleph235 <akram239911@gmail.com>
2022-10-06 11:36:09 +0200

    the sky

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,4 @@
 # Make limit 4000
 exec { 'fix limit':
-  command => 'sed -i 's/15/4096/g' /etc/default/nginx && sudo service nginx restart',
-  path    => '/usr/local/bin/:/bin/'
+  command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx && sudo service nginx restart",
 }
